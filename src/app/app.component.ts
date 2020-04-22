0dc323b019f18a53713e27d872ce34cdd34f09ab
Steven Garcia Castiblanco <steven.garcia@correo.usa.edu.co>
2020-04-21 20:34:11 -0500

    Version 1 Front IRSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'IRSS';
+  source: string[] = [];
+  onChange: EventEmitter<File>[] = [];
+  selectedFile: File[] = []
+  nombres: string[] = []
+  progreso: number[]=[]
+  email = new FormControl('', [Validators.required, Validators.email]);
+  correo: string = ""
+
+  onFileChanged(event) {
+    for (const iterator of event.target.files) {
+      this.selectedFile.push(iterator)
+      this.nombres.push(iterator.name)
+      this.progreso.push(0)
+      this.projectImage(iterator)
+      
+    }
+    console.log(this.selectedFile)
+  }
+
+  constructor(private http: HttpClient) { }
+
+  projectImage(file: File) {
+    var emit = new EventEmitter<File>()
+    let reader = new FileReader;
+    reader.onload = (e: any) => {
+      this.source.push(e.target.result);
+      emit.emit(file);
+      this.onChange.push(emit)
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  onUpload() {
+   
+    console.log(this.email.value)
+    if (!this.email.hasError('email') && !this.email.hasError('required')) {
+      const uploadData = new FormData();
+      for (const iterator of this.selectedFile) {
+        var envio =JSON.stringify({ 
+                                    myFile:this.source[this.selectedFile.indexOf(iterator)],
+                                    name: iterator.name, 
+                                    Correo:this.email.value
+                                  })
+        this.http.post('https://qec4hcotsk.execute-api.us-east-1.amazonaws.com/', envio, {
+          reportProgress: true,
+          observe: 'events'
+        })
+          .subscribe(event => {
+            this.progreso[this.selectedFile.indexOf(iterator)]=(event["loaded"]/event["total"])*100
+            if(this.progreso[this.selectedFile.indexOf(iterator)]===100){
+              this.Borrar(this.source[this.selectedFile.indexOf(iterator)])
+            }
+            console.log(event);
+          });
+      }
+    }
+  }
+
+  Borrar(item) {
+    var i = this.source.indexOf(item)
+    this.source.splice(i, 1);
+    this.selectedFile.splice(i, 1);
+    this.nombres.splice(i, 1)
+
+  }
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'El Correo Es Requerido';
+    }
+    return this.email.hasError('email') ? 'Correo No Valido' : '';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http'
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MaterialFileInputModule } from 'ngx-material-file-input';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input'; 
+import {MatExpansionModule} from '@angular/material/expansion';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatProgressBarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatIconModule,
+    MatDividerModule,
+    MatCardModule,
+    HttpClientModule,
+    MaterialFileInputModule,
+    MatFormFieldModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
